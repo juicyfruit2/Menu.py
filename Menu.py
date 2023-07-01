8b7ddac02bff0466e67abac60037c9e428f26ca6
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-07-01 14:56:55 +0200

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,9 @@
+item1 = input('What is the 1st item of choice ? :')
+item2 = input('what is 2nd item of choice ? :')
+item3 = input('what is your 3rd item of choice ? :')
+
+print('Order Confirmation! You have ordered')
+print(item1)
+print(item2)
+print(item3)
+
